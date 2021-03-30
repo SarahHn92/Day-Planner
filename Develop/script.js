b90fafcef7195a875bc849c2e5811473e0b250a8
Sarah Hinton <smcfarland2@gmail.com>
2021-03-30 18:29:35 +1100

    hours added to time boxes and centred

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,13 @@
 // add today's date
 
-//
+console.log("Hi there");
+
+var today = moment();
+$("#currentDay").text(today.format("MMM Do, YYYY"));
+console.log(today);
+
+// hook up save buttons to local storage
+var saveBtn = $(".btn");
+
+//function
+
