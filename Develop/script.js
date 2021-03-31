76d0bc2b20a5e878d3d551368446203285b38040
Sarah Hinton <smcfarland2@gmail.com>
2021-03-31 13:29:34 +1100

    working on Save button

--- Develop/script.js
+++ Develop/script.js
@@ -7,7 +7,13 @@ $("#currentDay").text(today.format("MMM Do, YYYY"));
 console.log(today);
 
 // hook up save buttons to local storage
-var saveBtn = $(".btn");
+var saveBtn = $(".btn" + ".text");
+var textInput = $(".text");
+console.log(textInput);
 
+$(saveBtn).click(localStorage.setItem) ;
+// {
+//     localStorage.textInput;
+// };
 //function
 
